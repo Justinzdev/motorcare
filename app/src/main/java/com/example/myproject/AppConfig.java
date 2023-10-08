ba378e91d4550a60d059ff4b6363644abab6542c
JDz <49568707+Justinzdev@users.noreply.github.com>
2023-10-08 21:12:24 +0700

    Update AppConfig.java

--- app/src/main/java/com/example/myproject/AppConfig.java
+++ app/src/main/java/com/example/myproject/AppConfig.java
@@ -2,5 +2,5 @@ package com.example.myproject;
 
 public class AppConfig {
     public static final String BASE_URL = "http://192.168.0.102:4000/";
-    public static final String GoogleAPI = "AIzaSyB6F4H_UYooAcsEPnds7b_hqiL70UPxI2o";
+    public static final String GoogleAPI = "";
 }
